a8adece69a05ca40fa18767835648aeadd90f0ca
Max Raffel <44725991+maxraffel@users.noreply.github.com>
2025-04-21 18:56:50 -0700

    initial try

--- wordle.cpp
+++ wordle.cpp
@@ -26,3 +26,32 @@ std::set<std::string> wordle(
 }
 
 // Define any helper functions here
+
+void wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    std::string current,
+    const int maxLength,
+    std::set<std::string>& out)
+{
+    if (current.length() == maxLength) {
+        if (dict.find(current) == dict.end()) {
+            return;
+        }
+        for (int i = 0; i < floating.length(); ++i) {
+            if (current.find(floating[i]) == std::string::npos) {
+                return;
+            }
+        }
+        // we will never make a choice that does not match the in string, so don't need to check
+        out.insert(current);
+    }
+
+    for (int i = 0; i < 26; ++i) {
+        char c = 'a' + i;
+        if (in[current.length()] == '-' || in[current.length()] == c) { // only make valid choices, but ignores the floating chars
+            wordleHelper(in, floating, dict, current + c, maxLength, out);
+        }
+    }
+}
